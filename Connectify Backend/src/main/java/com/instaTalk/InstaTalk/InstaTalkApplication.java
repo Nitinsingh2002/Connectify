12a9b4aab10168d06ba1f8cf61eb447d1341443b
Nitin kumar singh <singhnitinkumar145@gmail.com>
2024-12-02 22:56:28 +0530

    first commit

---
+++ Connectify Backend/src/main/java/com/instaTalk/InstaTalk/InstaTalkApplication.java
@@ -0,0 +1,13 @@
+package com.instaTalk.InstaTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaTalkApplication.class, args);
+	}
+
+}
---
+++ Connectify Backend/src/test/java/com/instaTalk/InstaTalk/InstaTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instaTalk.InstaTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
